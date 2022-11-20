8c4d0b4d7f309a13dead7345b63c616b1ed83c72
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2022-11-20 16:24:15 -0300

    Novo app HelloWord for console

---
+++ ConsoleHello/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleHello
+{
+    class ConsoleHello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+            string texto = Console.ReadLine()!;
+            Console.WriteLine(texto);
+
+        }
+    }
+}
