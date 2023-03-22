c174145070a9084dad3cf91dbc9bd84e806137f2
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-03-22 17:44:45 -0300

    Exemplo Curso C#

---
+++ Operadores/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 5;
+int b = 6;
+
+if ((a == 4) || (b == 6)){
+    Console.WriteLine("Verdadeiro em ambos");
+} else {
+    Console.WriteLine("Falso no primeiro");
+}
+
+
+if ((a == 4) | (b == 6)){
+    Console.WriteLine("Verdadeiro em 1 dos termos");
+} else {
+    Console.WriteLine("Falso nos dois termos");
+}
+
+Console.ReadKey();
