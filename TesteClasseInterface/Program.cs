89b0467ccd2de5e87f5acc58125675cab9431c90
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-03-28 21:35:21 -0300

    Teste de Classes

---
+++ TesteClasseInterface/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+interface Teste
+{
+    public string Mostrar();
+}
+
+class cTeste : Teste
+{
+    public string Mostrar(){
+        Console.WriteLine("Classe Teste");
+        return "Meu novo teste";
+    }
+
+    public static Teste Nova(){
+      return new cTeste();
+    }
+}
+
+public static void Main(string[] Args){
+  string oTeste = cTeste.Nova().Mostrar();
+  Console.WriteLine(oTeste);
+  
+}
+}
+
+
