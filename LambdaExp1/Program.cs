b9683839e7dba78a771e9d5cea9747494be58333
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-04-16 19:34:56 -0300

    Expressões Lambda

--- LambdaExp1/Program.cs
+++ LambdaExp1/Program.cs
@@ -5,8 +5,14 @@ class Program
     public static void Main(string[] args)
     {
         WriteLine(Teste());
-
-
-        string Teste() => "Olá usuário";
+        Write("\n Digite um número qualquer: ");
+        int num = int.Parse(GetValue(ReadLine()));
+        
+        WriteLine(Teste2(num));
+        ReadKey();        
     }
+
+    private static string GetValue(string? s) => s == null || s == string.Empty ? "0" : s;
+    private static string Teste2(int n) => (n % 2 == 0) ? "É Par" : "É Impar";
+    private static string Teste() => "Olá usuário";
 }
