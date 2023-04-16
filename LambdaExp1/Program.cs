ad5942bef00520c91ff3bdf33988a54c8eaebb99
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-04-16 18:44:55 -0300

    Experiências com Blazor e C# (lambda expressions)

---
+++ LambdaExp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine(Teste());
+
+
+        string Teste() => "Olá usuário";
+    }
+}
