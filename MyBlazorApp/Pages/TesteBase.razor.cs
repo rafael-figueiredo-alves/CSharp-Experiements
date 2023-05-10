f284a337b18dba4c4d1960544d8c412cbd8f8a8d
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-05-10 18:02:39 -0300

    Estudando Blazor

---
+++ MyBlazorApp/Pages/TesteBase.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MyBlazorApp.Pages
+{
+    public class TesteBase :ComponentBase
+    {
+        public bool Exibir = false;
+        protected string Texto = string.Empty;
+
+        public void Clicou()
+        {
+            Exibir = !Exibir;
+        }
+    }
+}
