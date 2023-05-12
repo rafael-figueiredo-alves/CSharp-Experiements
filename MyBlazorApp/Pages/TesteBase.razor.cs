479accee0c2cb90ceb8c24fd930b6634ea0b4d86
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-05-12 15:19:01 -0300

    Testando Blazor

--- MyBlazorApp/Pages/TesteBase.razor.cs
+++ MyBlazorApp/Pages/TesteBase.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace MyBlazorApp.Pages
 {
@@ -7,9 +8,41 @@ namespace MyBlazorApp.Pages
         public bool Exibir = false;
         protected string Texto = string.Empty;
 
+        [Inject]
+        public IJSRuntime Js { get; set; }
+
+        [Parameter]
+        public int Id { get; set; }
         public void Clicou()
         {
             Exibir = !Exibir;
         }
-    }
+
+		protected override async void OnAfterRender(bool firstRender)
+		{
+			base.OnAfterRender(firstRender);
+			//await Js.InvokeVoidAsync("window.alert", "Esta mensagem foi definida no C# !");
+		}
+
+        protected void Atualizar()
+        {
+            StateHasChanged();
+        }
+
+        private string Chave = "MeuApp";
+        protected async Task Gravar()
+        {
+            await Js.InvokeVoidAsync("window.localStorage.setItem", Chave, Texto);
+        }
+
+        protected async Task Ler()
+        {
+            await Js.InvokeVoidAsync("alert", await Js.InvokeAsync<string>("window.localStorage.getItem", Chave));
+        }
+
+        protected async void Remover()
+        {
+			await Js.InvokeVoidAsync("window.localStorage.removeItem", Chave);
+		}
+     }
 }
