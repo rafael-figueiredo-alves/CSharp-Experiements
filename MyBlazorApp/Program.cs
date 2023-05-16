26c5ead2650f321a8608b372853f1ed0791a1876
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-05-16 12:01:00 -0300

    Experimentos em Blazor

--- MyBlazorApp/Program.cs
+++ MyBlazorApp/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyBlazorApp;
+using MyBlazorApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<IAluno, Aluno>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
