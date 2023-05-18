82b11d97b28cfe27385fb8992508ae11d492afc8
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-05-18 13:55:15 -0300

    Learning Blazor and C# - WebApi

---
+++ WebApplicationAPI/Services/Alunos.cs
@@ -0,0 +1,48 @@
+﻿using WebApplicationAPI.Models;
+
+namespace WebApplicationAPI.Services
+{
+    public class Alunos : IAlunos
+    {
+        private List<Aluno>? alunos;
+
+        public Alunos()
+        {
+            alunos = new List<Aluno>()
+            {
+                new Aluno() { Id = 1, Nome = "Davi Alves" },
+                new Aluno() { Id = 2, Nome = "Rafaela Alves" },
+                new Aluno() { Id = 3, Nome = "Jailza Rocha Alves" }
+            };
+        }
+        public void AddAluno(Aluno aluno)
+        {
+            alunos!.Add(aluno);
+        }
+
+        public void DeleteAluno(int id)
+        {
+            Aluno AlunoRemover = GetAluno(id)!;
+            if (AlunoRemover != null)
+            {
+                _ = alunos!.Remove(AlunoRemover);
+            }
+        }
+
+        public Aluno? GetAluno(int id)
+        {
+            return alunos!.FirstOrDefault(Al => Al.Id == id);
+        }
+
+        public List<Aluno> GetAlunos()
+        {
+            return alunos!;
+        }
+
+        public void UpdateAluno(Aluno aluno)
+        {
+            DeleteAluno(aluno.Id);
+            AddAluno(aluno);
+        }
+    }
+}
---
+++ WebApplicationAPI/Services/IAlunos.cs
@@ -0,0 +1,13 @@
+﻿using WebApplicationAPI.Models;
+
+namespace WebApplicationAPI.Services
+{
+    public interface IAlunos
+    {
+        public List<Aluno> GetAlunos();
+        public Aluno? GetAluno(int id);
+        public void AddAluno(Aluno aluno);
+        public void UpdateAluno(Aluno aluno);
+        public void DeleteAluno(int id);
+    }
+}
