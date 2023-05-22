5d11a091851cc6a20147610ba6c803ee3f873cbd
Rafael de Figueiredo Alves <58051735+rafael-figueiredo-alves@users.noreply.github.com>
2023-05-22 09:56:55 -0300

    Experimentos diversos
    - Code-behind
    - Documentação de API

--- WebApplicationAPI/Program.cs
+++ WebApplicationAPI/Program.cs
@@ -11,8 +11,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-	options.SwaggerDoc("v2", new OpenApiInfo
+	options.SwaggerDoc("Desk web API v1", new OpenApiInfo
     {    
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    options.SwaggerDoc("Desk web API v2", new OpenApiInfo
+    {
         Version = "v2",
         Title = "ToDo API",
         Description = "An ASP.NET Core Web API for managing ToDo items",
@@ -38,7 +55,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI(options =>
     {
-        options.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        options.SwaggerEndpoint("/swagger/Desk web API v1/swagger.json", "Desk web API v1");
+        options.SwaggerEndpoint("/swagger/Desk web API v2/swagger.json", "Desk web API v2");
     });
 }
 
